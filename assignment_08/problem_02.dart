3cb947fb8c6058318e683bf7cbd36e21dfbb19a0
Mahafujmr <mrtuhin444@gmail.com>
2024-11-24 10:08:07 +0530

    dart assigntment

--- assignment_08/problem_02.dart
+++ assignment_08/problem_02.dart
@@ -1,1 +1,33 @@
-//
+// 2. Encapsulated Bank Account
+class BankAccount {
+  // class name = BankAccount
+  double _balance = 0;
+  // double balance
+
+// use method and creation method name = deposit
+// parameter = double amount
+  void deposit(double amount) {
+    // _balance = private variable
+    _balance += amount;
+  }
+
+  // method =withdraw and parameter = double amount
+  void withdraw(double amount) {
+    //use if else  statement
+    if (_balance >= amount) {
+      _balance -= amount;
+    } else {
+      print("Insufficient balance");
+    }
+  }
+
+  double get balance => _balance;
+}
+
+void main() {
+  // creation object = account
+  BankAccount account = BankAccount(); // calling class name
+  account.deposit(1000); // access object and method
+  account.withdraw(500);
+  print(account.balance); // Output: 500
+}
